b2c674a773e3361cc764b991dff3648ddc305790
Alan Maldonado <business.alanm@gmail.com>
2019-05-01 00:17:53 -0500

    hotfix-1

---
+++ block.py
@@ -0,0 +1,31 @@
+# Author: Alan Maldonado
+# Description: Blocks Module
+
+# *** Libraries ***
+
+import sys
+import pygame
+
+
+class Block(object):
+    rows = 50
+    w = 1000
+
+    def __init__(self, defPos, x=1, y=0, color=(177, 183, 0)):
+        self.pos = defPos
+        self.ownX = x
+        self.ownY = y
+        self.color = color
+
+    def move(self, x, y):
+        self.ownX = x
+        self.ownY = y
+        self.pos = (self.pos[0] + self.ownX, self.pos[1] + self.ownY)
+
+    def render(self, surface, eyes=False):
+        dt = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.color,
+                         (i*dt+1, j*dt+2, dt-2, dt-2))
