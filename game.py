198992bc8f37f000d3365cc68097d184af2dbb09
Alan Maldonado <business.alanm@gmail.com>
2019-04-30 21:44:17 -0500

    game-engine-init

---
+++ game.py
@@ -0,0 +1,14 @@
+# Author: Alan Maldonado
+# Class: Plugin Development
+# Description: Snake Game on PyGame
+
+# *** Libraries ***
+import sys
+import pygame as engine
+
+# Initialize the game engine and properties declaration
+engine.init()
+WindowConfig = width, height = 1000, 1000
+
+# Initialize window
+window = engine.display.set_mode(WindowConfig)
