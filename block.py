79326861c104b29187dd8d7773d07978123c0fc9
Alan Maldonado <business.alanm@gmail.com>
2019-05-19 20:45:01 -0500

    block-and-snake-docs-finished

--- block.py
+++ block.py
@@ -6,9 +6,13 @@
 import sys
 import pygame
 
+# Block Class
+# Contains: - In-game Objects to Render
+
 
 class Block(object):
 
+    # Block Class Initializer
     def __init__(self, defPos, x=1, y=0, color=(177, 183, 0), rows=50, w=1000):
         self.rows = rows
         self.w = w
@@ -17,11 +21,13 @@ class Block(object):
         self.ownY = y
         self.color = color
 
+    # Grid Moving Method
     def move(self, x, y):
         self.ownX = x
         self.ownY = y
         self.pos = (self.pos[0] + self.ownX, self.pos[1] + self.ownY)
 
+    # Block rendering per object placed.
     def render(self, surface, eyes=False):
         dt = self.w // self.rows
         i = self.pos[0]
